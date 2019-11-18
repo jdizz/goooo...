f1465a1ad36c118af8484a86c3262182eacf6051
jdizz <jdizz@users.noreply.github.com>
2019-11-18 10:52:51 -0800

    Added a few comments

--- hello-world/hello-world.go
+++ hello-world/hello-world.go
@@ -1,8 +1,15 @@
+/*
+	Hello World
+	Created: 11/18/19
+*/
+
+// Package declaration
 package main
 
+// Import format package for I/O
 import "fmt"
 
-// Print Hello World
+// Print Hello World!
 func main() {
 	fmt.Println("Hello World!")
 }
